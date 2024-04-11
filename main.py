f3d4764281c04412b6a47729396cc395ebbd0560
Rahizo <45150023+Rahizo@users.noreply.github.com>
2024-04-11 14:41:15 -0400

    starting actual project

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+# Khizar Khan
 
 num = input("give number")
 num2 = ""
-for digit in num:
 
-    newDigitInit = str((int)digit + 3)
-    print(newDigitInit)
+def encode(num_string : str) -> str:
+    encoded = ""
+    for char in num_string:
+        encoded += str(int(char) + 3 % 10)
+        return encoded
+
+def main():
+    op = 1
+    while op in range(1,3):
+        print("Encoder")
+        print("------------------")
+        print("0. Exit")
+        print("1. Encode")
+        print("2. Decode")
+
+        op = int(input("Select an option: "))
