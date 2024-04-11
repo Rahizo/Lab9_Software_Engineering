f5ad6fe4c8535d02b817a53af0f29a01b61500fb
Rahizo <45150023+Rahizo@users.noreply.github.com>
2024-04-11 15:06:24 -0400

    changed to exactly match example output

--- main.py
+++ main.py
@@ -11,24 +11,31 @@ def encode(num_string: str) -> str:
 def main():
     op = 1
     while op in range(1, 3):
-        print("Encoder")
-        print("------------------")
-        print("0. Exit")
+        print("Menu")
+        print("-------------")
         print("1. Encode")
         print("2. Decode")
+        print("3. Exit")
 
-        op = int(input("Select an option: "))
+        op = int(input("Please enter an option: "))
 
         if op == 0:
             break
         elif op == 1:
-            num_string = input("enter a password to encode: ")
-            print(f"Original password: {num_string}")
-            print("New password: " + encode(num_string))
+            num_string = input("Please enter your password to encode:  ")
+            # print(f"Original password: {num_string}")
+            newString = encode(num_string)
+            print("Your password has been encoded and stores!")
+            # print("New password: " + encode(num_string))
         elif op == 2:
             num_string = input("enter a password to decode: ")
-            print(f"Orignal password : {num_string}")
+            # print(f"Orignal password : {num_string}")
             # print(f"Decoded password: " + decode(num_string))
+            decodedString = decode(num_string)
+            print("The encoded password is " + num_string + ", and the original password is " +
+                  decode(num_string))
+        else:
+            break
 
 
 if __name__ == "__main__":
