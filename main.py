957f4a5d31423b6118998159a70de9c10e50b2d4
Rahizo <45150023+Rahizo@users.noreply.github.com>
2024-04-11 14:48:13 -0400

    if __name__ added + functionality fixed in elif 1

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 # Khizar Khan
 
-num = input("give number")
-num2 = ""
 
-def encode(num_string : str) -> str:
+def encode(num_string: str) -> str:
     encoded = ""
     for char in num_string:
-        encoded += str(int(char) + 3 % 10)
-        return encoded
+        encoded += str((int(char) + 3) % 10)
+    return encoded
+
 
 def main():
     op = 1
-    while op in range(1,3):
+    while op in range(1, 3):
         print("Encoder")
         print("------------------")
         print("0. Exit")
@@ -19,3 +18,18 @@ def main():
         print("2. Decode")
 
         op = int(input("Select an option: "))
+
+        if op == 0:
+            break
+        elif op == 1:
+            num_string = input("enter a password to encode: ")
+            print(f"Original password: {num_string}")
+            print("New password: " + encode(num_string))
+        elif op == 2:
+            num_string = input("enter a password to decode: ")
+            print(f"Orignal password : {num_string}")
+            # print(f"Decoded password: " + decode(num_string))
+
+
+if __name__ == "__main__":
+    main()
